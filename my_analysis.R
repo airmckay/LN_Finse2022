3a9ee2a8e234907f26609d9a2a1491bba0b6606d
airmckay <apmc@nmbu.no>
2022-11-17 09:19:27 +0100

    load packages

--- my_analysis.R
+++ my_analysis.R
@@ -1,6 +1,15 @@
 
+
+###############################
+## Set up git hub connection ##
+
 library(usethis)
 use_github() 
 git_default_branch_rename() 
 
 git_default_branch_configure(name = "main")
+
+###############################
+## load packages ##
+
+library(tidyverse)
